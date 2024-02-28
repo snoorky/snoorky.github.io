16fc0197f364ba0031ce679b6500fd4410764ce3
sno-orky <raphaelvitorlopes@icloud.com>
2024-02-28 17:05:32 -0300

    Robots, Schema and OP codes

---
+++ assets/js/schema.js
@@ -0,0 +1,14 @@
+function getSchemaData() {
+    return {
+        "@context": "http://schema.org",
+        "@type": "Person",
+        "name": "Raphael Vitor",
+        "url": "https://snoorky.github.io/",
+        "sameAs": [
+            "https://www.facebook.com/raphaelvitorlopes",
+            "https://www.instagram.com/sno.orky/",
+            "https://www.linkedin.com/in/raphaelvitor/",
+            "https://github.com/sno-orky/"
+        ]
+    };
+}
