f50bb8837fb12e2d61bc60dbfe8d704e5f166086
Raphael Vitor <raphaelvitorlopes@icloud.com>
2024-12-07 14:26:46 -0300

    chore: move Google Analytics tag to separate JS file"

---
+++ analytics.js
@@ -0,0 +1,14 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-95DDYX6J01';
+    script.async = true;
+    document.head.appendChild(script);
+  
+    script.onload = function() {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-95DDYX6J01');
+    };
+  })();
+  
